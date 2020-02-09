9a2aa63f5f9c7a0e8c43352be3cafed0dde79c01
Leo Saglia <leosaglia@hotmail.com>
2020-02-08 21:27:41 -0300

    Aplicado upload de arquivos/avatar com multer, realizado relacionamento tabela users + files

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -27,6 +27,11 @@ class User extends Model {
         return this;
     }
 
+    // Relacionamento entre models/tabelas; FK
+    static associate(models) {
+        this.belongsTo(models.File, { foreignKey: 'avatar_id' });
+    }
+
     checkPassword(password) {
         return bcrypt.compare(password, this.password_hash);
     }
