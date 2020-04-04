138484824052c57c17c82f626d0f1dc11904ee56
Leo Saglia <leosaglia@hotmail.com>
2020-04-03 22:59:11 -0300

    feat(frontend): Apply unform (Rocketseat) on the forms

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import Logo from '~/assets/logo.svg';
 
 export default function SignIn() {
+	function handleSubmit(data) {
+		console.tron.log(data);	
+	}
+
 	return (
 		<>
 			<img src={Logo} alt="GoBarber" />
 
-			<form action="">
-				<input type="email" placeholder="Seu e-mail" />
-				<input type="password" placeholder="Sua senha secreta" />
+			<Form onSubmit={handleSubmit}>
+				<Input name="email" type="email" placeholder="Seu e-mail" />
+				<Input name="password" type="password" placeholder="Sua senha secreta" />
 
 				<button type="submit">Acessar</button>
 				<Link to="/register">Criar conta gratuita</Link>
-			</form>
+			</Form>
 		</>
 	);
 }
--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import Logo from '~/assets/logo.svg';
 
 export default function SignUp() {
+	function handleSubmit(data) {
+		console.tron.log(data);
+	}
+
 	return (
 		<>
 			<img src={Logo} alt="GoBarber" />
 
-			<form action="">
-				<input placeholder="Nome completo" />
-				<input type="email" placeholder="Seu e-mail" />
-				<input type="password" placeholder="Sua senha secreta" />
+			<Form onSubmit={handleSubmit}>
+				<Input name="name" placeholder="Nome completo" />
+				<Input name="email" type="email" placeholder="Seu e-mail" />
+				<Input name="password" type="password" placeholder="Sua senha secreta" />
 
 				<button type="submit">Criar conta</button>
 				<Link to="/">Já possuo login</Link>
-			</form>
+			</Form>
 		</>
 	);
 }
