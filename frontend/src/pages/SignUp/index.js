505af6b8640417aa1ef3082d9a65c8d448a42190
Leo Saglia <leosaglia@hotmail.com>
2020-04-04 22:04:23 -0300

    feat(frontend): Add provider register

--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,24 +1,28 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 import Logo from '~/assets/logo.svg';
 
+import { signUpRequest } from '~/store/modules/auth/actions';
+
 const schema = Yup.object().shape({
-	name: Yup.string()
-		.required('O nome é obrigatório'),
+	name: Yup.string().required('O nome é obrigatório'),
 	email: Yup.string()
 		.email('Insira um e-mail válido')
 		.required('O e-mail é obrigatório'),
 	password: Yup.string()
 		.min(6, 'A senha requer no mínimo 6 caracteres')
-		.required('A senha é obrigatória')
+		.required('A senha é obrigatória'),
 });
 
 export default function SignUp() {
-	function handleSubmit(data) {
-		console.tron.log(data);
+	const dispatch = useDispatch();
+
+	function handleSubmit({ name, email, password }) {
+		dispatch(signUpRequest(name, email, password));
 	}
 
 	return (
@@ -28,7 +32,11 @@ export default function SignUp() {
 			<Form schema={schema} onSubmit={handleSubmit}>
 				<Input name="name" placeholder="Nome completo" />
 				<Input name="email" type="email" placeholder="Seu e-mail" />
-				<Input name="password" type="password" placeholder="Sua senha secreta" />
+				<Input
+					name="password"
+					type="password"
+					placeholder="Sua senha secreta"
+				/>
 
 				<button type="submit">Criar conta</button>
 				<Link to="/">Já possuo login</Link>
