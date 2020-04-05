91ea4367047c1110abd23c25b6fad7d8d9b7da62
Leo Saglia <leosaglia@hotmail.com>
2020-04-05 16:38:43 -0300

    feat(frontend): Add Notification funcionality

--- frontend/src/components/Notifications/styles.js
+++ frontend/src/components/Notifications/styles.js
@@ -29,6 +29,7 @@ export const Badge = styled.button`
 `;
 
 export const NotificationList = styled.div`
+	display: ${(props) => (props.visible ? 'block' : 'none')};
 	position: absolute;
 	width: 260px;
 	left: calc(50% - 130px);
@@ -70,8 +71,10 @@ export const Notification = styled.div`
 	}
 
 	time {
+		display: block;
 		font-size: 12px;
 		opacity: 0.6;
+		margin-bottom: 5px;
 	}
 
 	button {
@@ -79,9 +82,6 @@ export const Notification = styled.div`
 		border: 0;
 		background: none;
 		color: ${lighten(0.2, '#7159c1')};
-		padding: 0 5px;
-		margin: 0 5px;
-		border-left: 1px solid rgba(255, 255, 255, 0.1);
 	}
 
 	${(props) =>
@@ -94,6 +94,7 @@ export const Notification = styled.div`
 				height: 8px;
 				background: #ff892e;
 				border-radius: 50%;
+				margin-left: 10px;
 			}
 		`}
 `;
